49c00e65292e9777e200bb780db83e6671a92694
Dario Zubaray <dario.zubaray@repairsmith.com>
2022-08-18 11:25:01 -0300

    adding swr and fetching data in homepage from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,25 @@ import type { NextPage } from 'next';
 import { Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
 
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
+
+  const { data, error } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
         <ProductList 
-          products={ initialData.products as any }
+          products={ data.products as any }
         />
     
 
@@ -21,4 +28,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default HomePage
