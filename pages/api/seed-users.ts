fd0f9a279e9eddb38a3ac53724ca6c7c313abbdf
Dario Zubaray <dario.zubaray@repairsmith.com>
2022-08-23 17:38:25 -0300

    adding user-seed

--- pages/api/seed-users.ts
+++ pages/api/seed-users.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+
 import { db, seedDatabase } from '../../database';
-import { Product, User } from '../../models';
+import { User } from '../../models';
 
 type Data = { message: string }
 
@@ -12,10 +13,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     await db.connect();
     await User.deleteMany();
-    await User.insertMany( seedDatabase.initialData.users )
-    await Product.deleteMany();
-    await Product.insertMany( seedDatabase.initialData.products );
+    await User.insertMany( seedDatabase.initialData.users );
     await db.disconnect();
 
-    res.status(200).json({ message: 'Proceso realizado correctamente' });
+    res.status(200).json({ message: 'Proceso de usuarios realizado correctamente' });
 }
