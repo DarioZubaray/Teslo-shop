713eb55873509f0e89b161981278e3a19ef1b8e5
Dario Zubaray <dario.zubaray@repairsmith.com>
2022-08-27 14:23:40 -0300

    update Order model and interface

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -57,6 +57,8 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         // Generar order de compra
         const userId = session.user._id;
         const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+        newOrder.total = Math.round( newOrder.total * 100 ) /100;
+
         await newOrder.save();
         await db.disconnect();
 
