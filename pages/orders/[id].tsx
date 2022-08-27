374481b7b4b9b31880d1383c9206a4dea8a794a6
Dario Zubaray <dario.zubaray@repairsmith.com>
2022-08-27 15:09:31 -0300

    adding paypal button

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,8 +1,8 @@
-import NextLink from 'next/link';
 import { GetServerSideProps, NextPage } from 'next';
 import { getSession } from 'next-auth/react';
+import { PayPalButtons } from "@paypal/react-paypal-js";
 
-import { Link, Box, Card, CardContent, Divider, Grid, Typography, Chip } from '@mui/material';
+import { Box, Card, CardContent, Divider, Grid, Typography, Chip } from '@mui/material';
 import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
 
 import { ShopLayout } from '../../components/layouts/ShopLayout';
@@ -86,7 +86,24 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                                             icon={ <CreditScoreOutlined /> }
                                         />
                                     ) : (
-                                        <h1>Pagar</h1>
+                                        <PayPalButtons
+                                            createOrder={(data, actions) => {
+                                                return actions.order.create({
+                                                    purchase_units: [
+                                                        {
+                                                            amount: {
+                                                                value: `${ order.total }`,
+                                                            },
+                                                        },
+                                                    ],
+                                                });
+                                            }}
+                                            onApprove={(data, actions) => {
+                                                return actions.order!.capture().then((details) => {
+                                                    console.log({details})
+                                                });
+                                            }}
+                                        />
                                     )
                                 }
                             </Box>
